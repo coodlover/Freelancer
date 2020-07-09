c5ca80e3130dd1b8c533559e2b3ead47a8a51b8e
coodlover <1594487572@qq.com>
2020-07-09 11:27:57 +0800

    add budget

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -87,9 +87,11 @@ def getDetail(html, url):
     detailDic = dict()
     if not soup.find(class_='PageProjectViewLogout-header-title'):
         return None
+    detailDic['budget'] = soup.find(class_='PageProjectViewLogout-header-byLine').get_text()[3:]
     detailDic['proName'] = soup.find(class_='PageProjectViewLogout-header-title').string
     detailDic['url'] = url
     print(detailDic['proName'], detailDic['url'], end=" ")
+    print(detailDic['budget'], end=" ")
     proDescription = ""
     for s in soup.find(class_='PageProjectViewLogout-detail').stripped_strings:
         proDescription += s + " "
@@ -161,8 +163,8 @@ def mysqlWrite(detailDic, db, cursor):
             sql = "update `developer` set `project`=CONCAT(`project`, %s) where `url`=%s"
             params = (detailDic['url']+";", devDic['url'])
             sqlExe(db, cursor, sql, params)
-    sql = "INSERT INTO `PROJECT`(url, name, tag, description, `match`) VALUES (%s,%s,%s,%s,%s)"
-    params = (detailDic['url'], detailDic['proName'], detailDic['proTag'], detailDic['proDescription'], match)
+    sql = "INSERT INTO `PROJECT`(url, name, budget, tag, description, `match`) VALUES (%s,%s,%s,%s,%s)"
+    params = (detailDic['url'], detailDic['proName'], detailDic['budget'], detailDic['proTag'], detailDic['proDescription'], match)
     sqlExe(db, cursor, sql, params)
     print("写入完成")
 
@@ -205,7 +207,7 @@ def crawler(page, num):
             if (i == page) and (n < num):
                 n += 1
                 continue
-            print("{}/{} ".format(n, total), end=" ")
+            print("{}/{} ".format(n, total))
             html = getHTML("https://www.freelancer.cn"+url)
             if html == "":
                 exit_error(db, i, n)
